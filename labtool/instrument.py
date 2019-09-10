3944e3e85c9e50f80a232ce217bdd4437dbd0251
Lucas Kammann <lucaskammann@hotmail.com>
2019-09-10 15:29:30 -0300

    Instrument constructor added, Oscilloscope created!

--- labtool/instrument.py
+++ labtool/instrument.py
@@ -2,9 +2,29 @@
 Instrument Base Class to define a common or standard way to identify instruments.
 """
 
+# third-party modules
+import pyvisa
+
+
+class ResourceNotFound(Exception):
+    def __init__(self):
+        super(ResourceNotFound, self).__init__(
+            "The given resource's name was not found in the Resource Manager"
+        )
+
 
 class Instrument(object):
     """ Instrument base class """
 
     IDN = "Instrument's IDN"
 
+    def __init__(self, resource_name):
+        """ A Resource is opened and its reference will be saved, but if there is
+        no resource with the given name or identifier, then an exception will be
+        raised. """
+
+        try:
+            resource_manager = pyvisa.ResourceManager()
+            self.resource = resource_manager.open_resource(resource_name)
+        except:
+            raise ResourceNotFound
---
+++ labtool/oscilloscope/base/oscilloscope.py
@@ -0,0 +1,21 @@
+"""
+Oscilloscope base class. It contains the base methods that should be defined
+or implemented by any child class defining a specific oscilloscope model.
+"""
+
+# python native modules
+from abc import ABC
+
+
+# labtool project modules
+from labtool.instrument import Instrument
+
+
+class Oscilloscope(Instrument, ABC):
+    """ Oscilloscope Base Class.
+    When inheriting to define a child class, static parameters are needed
+    to be recognized by the labtool """
+
+    # Oscilloscope static parameters
+    brand = "OscilloscopeBrand"
+    model = "OscilloscopeModel"
